8fdedbef099e515510f11db7a949992005b650d5
Dillo Raju <dilloshion@gmail.com>
2022-01-11 19:33:59 -0500

    fix sentry dependency error

--- frontend/src/lib/errorLib.js
+++ frontend/src/lib/errorLib.js
@@ -10,12 +10,6 @@ export function initSentry() {
 
   Sentry.init({
     dsn: config.SENTRY_DSN,
-    integrations: [new Integrations.BrowserTracing()],
-
-    // Set tracesSampleRate to 1.0 to capture 100%
-    // of transactions for performance monitoring.
-    // We recommend adjusting this value in production
-    tracesSampleRate: 1.0,
   });
 }
 
