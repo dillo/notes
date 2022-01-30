04d8ad24c342eaf379c324a1daee49391030e63e
Dillo Raju <dilloshion@gmail.com>
2022-01-30 10:52:20 -0500

    Refactor WIP

--- frontend/src/lib/awsLib.js
+++ frontend/src/lib/awsLib.js
@@ -1,8 +1,7 @@
 import { Storage } from "aws-amplify";
 
-export async function s3Upload(file) {
+export const s3Upload = async (file) => {
   const filename = `${Date.now()}-${file.name}`;
-
   const stored = await Storage.vault.put(filename, file, {
     contentType: file.type,
   });
